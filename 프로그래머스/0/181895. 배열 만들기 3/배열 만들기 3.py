d7b69f20f40c82624b794280514829c26bc73698
Vamos00 <117256782+Kimtae00@users.noreply.github.com>
2024-02-27 17:46:56 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -0,0 +1,9 @@
+def solution(arr, intervals):
+    
+    first_interval = arr[intervals[0][0]:intervals[0][1] + 1]
+    second_interval = arr[intervals[1][0]:intervals[1][1] + 1]
+
+    
+    new_array = first_interval + second_interval
+
+    return new_array
